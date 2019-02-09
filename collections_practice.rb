7a3a39eaabf37812dd4e03c1b4309674cc26772d
Adam Gordon <adamjgordon24@gmail.com>
2019-02-09 03:23:14 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,22 +1,23 @@
-# your code goes here
+require "pry"
+
 def begins_with_r (array)
   array.all? do |word|
-    word.start_with?("r")
+    word[0] == "r"
   end
 end
 
 def contain_a (array)
   array.select do |word|
     word.include?("a")
-  end
+    end
 end
 
 def first_wa(list)
-    list.find do |word|
-      if word.class == String
-        word.start_with?("wa")
-      end
-    end
+  list.find do |word|
+    if word.class == String
+    word.start_with?('wa')
+  end
+  end
 end
 
 def remove_non_strings(array)
@@ -26,49 +27,51 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-    array.uniq.each do |element1|
-      counter = 0
-      array.each do |element2|
-        if element1 == element2
-          counter += 1 
-        end
+  array.uniq.each do |i| count = 0
+      array.each do |i2| 
+        if i2 == i then count += 1 
       end
-      element1[:count] = counter
     end
+   i[:count] = count
+  end
 end
 
 
 def merge_data(keys, data)
-  merged_array = []
+  merged = []
   keys.each do |i| 
     data.first.collect do |key,value| 
-      if i.values[0] == key
-        merged_array << i.merge(value) 
-      end
-    end
-  end 
-  merged_array
+      if i.values[0] == key 
+        merged << i.merge(value) 
+     end 
+   end 
+  end
+  merged
 end
 
 
-def find_cool(hash)
-  hash.select do |member|
-    member.any? do |key,value|
-      value == "cool"
+def find_cool(array)
+  cool_array = []
+  array.each do |hash|
+    hash.each do |key, value|
+      if value == "cool"
+        cool_array << hash
+      end
     end
   end
+  cool_array
 end
 
 def organize_schools(schools)
     locations_hash = {}
     schools.collect do |key,value| 
-      locations_hash[value[:location]] = []
-    end 
+      locations_hash[value[:location]] = [] 
+      end
     locations_hash.each do |key,value| 
       schools.each do |key1,value1| 
-        if key == value1[:location] 
+        if key == value1[:location]
           value << key1  
         end
-      end 
+     end
     end
 end
